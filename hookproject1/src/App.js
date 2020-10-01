e99a09ad428138302c50c9efb78a7230cd149ee4
Madan-444 <mmadan3600@gmail.com>
2020-10-01 08:01:39 +0545

    one

--- hookproject1/src/App.js
+++ hookproject1/src/App.js
@@ -1,39 +1,60 @@
 import React, {useState,useReducer} from 'react';
 import './App.css';
+import Todo from './Todo'
 
 
-const ACTIONS = {
-  INCREMENT:'increment',
-  DECREMENT:'decrement'
+ export const ACTIONS = {
+  ADD_TODO: 'add_todo',
+  TOGGLE_TODO:'toggle',
+  DELETE_TODO:'delete'
 
 }
 
-const reducer = (state,action)=> {
-  switch(action.type) {
-    case ACTIONS.INCREMENT:
-      return {count:state.count +1}
-    case ACTIONS.DECREMENT: 
-      return { count: state.count -1}
-    default: 
-      return state
+const reducer = (todos,action)=> {
+  switch(action.type){
+    case ACTIONS.ADD_TODO:
+      return [...todos,newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if(todo.id === action.payload.id){
+          return {...todo,complete:!todo.complete}
+        }
+        return todo
+      })
+      case ACTIONS.DELETE_TODO: 
+         return todos.filter(todo=> todo.id !== action.payload.id)
+      default:
+        return todos
   }
 
 }
 
+function newTodo(name){
+  return {id: Date.now(),name:name,complete:true}
+}
+
+
 function App() {
+  
 
-  const [state,dispatch] = useReducer(reducer, {count:0})
+  const [todos,dispatch] = useReducer(reducer, [])
+  const [name,setName] = useState('')
 
-  const [count,setCount] = useState(0)
+  console.log(todos)
+
   return (
     <div className="App">
-      <>
-      <button onClick = {()=> dispatch({type:ACTIONS.INCREMENT})} >+</button>
-      <span> {state.count} </span>
-      <button onClick ={()=> dispatch({type:ACTIONS.DECREMENT})} >-</button>
-     
-      
-      </>
+      <form onSubmit = {(e)=> {
+        e.preventDefault()
+        dispatch({type:ACTIONS.ADD_TODO,payload: {name:name}})
+        setName('')
+      }}>
+        <input type='text' value={name} onChange= {e=> setName(e.target.value)} />
+      </form>
+      {todos.map(todo=> 
+        <Todo key={todo.id} todo = {todo} dispatch = {dispatch} />
+        )
+      }
     </div>
   );
 }
